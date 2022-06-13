c405db5a0be23e14fcb3675a218b5204f0c553e7
Peter JH. Lee <neworld0@gmail.com>
2022-06-13 14:53:40 +0900

    prod.py modified 6/13 14:53

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,5 +2,8 @@ from .base import *
 
 ALLOWED_HOSTS = ['3.39.26.240']
 STATIC_ROOT = BASE_DIR / "static/"
-STATICFILES_DIRS = []
+# STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 DEBUG = False
