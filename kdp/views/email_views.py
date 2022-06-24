c9c3f9b6c6247a8c9ef92f5317022d6eec385514
unknown <neworld0@gmail.com>
2022-06-24 23:47:04 +0900

    email sso code modified 6/24 23:46

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 import logging
 logger = logging.getLogger('kdp')
 
-def email(request):
-    logger.info("INFO 레벨로 출력")
-    return render(request, 'kdp/email.html')
+# def email(request):
+#     logger.info("INFO 레벨로 출력")
+#     return render(request, 'kdp/email.html')
+
+
+# Bulletin Board 상세내용 출력
+@login_required(login_url='common:login')
+def email(request, ):
+    user = User.objects.get(username=request.user)
+    context = {'cid': user, "MailToken": "0SF4RVoz51hPaa37sCYJdftrpk91eL6hBAKWzq3n"}
+    return render(request, 'kdp/email.html', context)
