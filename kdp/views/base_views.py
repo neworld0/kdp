945e4978bd1c3fb59ca110ad695c94e7e4179be6
unknown <neworld0@gmail.com>
2022-06-26 00:19:27 +0900

    SSO code added 6/26 00:19

--- kdp/views/base_views.py
+++ kdp/views/base_views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import math
 import logging
 logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info("INFO 레벨로 출력")
     return render(request, 'kdp/index.html')
+
+
+# # email sso 생성
+# @login_required(login_url='common:login')
+# def email(request):
+#     user = User.objects.get(username=request.user)
+#     token = makeid()
+#     context = {"cid": user, "MailToken": token}
+#     return render(request, 'http://mail.koreadigitalpark.com/mail_api/token_sso', context)
+#
+#
+# def makeid():
+#     text = ""
+#     possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+#     for i in range(0, 40):
+#         text += possible.charAt(math.floor(math.random() * possible.length))
+#     return text
--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+import math
+import random
 import logging
+import requests
+import json
 logger = logging.getLogger('kdp')
 
-# def email(request):
-#     logger.info("INFO 레벨로 출력")
-#     return render(request, 'kdp/email.html')
-
-
-# Bulletin Board 상세내용 출력
 @login_required(login_url='common:login')
-def email(request, ):
-    user = User.objects.get(username=request.user)
-    context = {"cid": user, "MailToken": "0SF4RVoz51hPaa37sCYJdftrpk91eL6hBAKWzq3n"}
-    return render(request, 'kdp/email.html', context)
+def email(request):
+    logger.info("INFO 레벨로 출력")
+    return render(request, 'kdp/email.html')
+
+
+# # email sso 생성
+# @login_required(login_url='common:login')
+# def email(request):
+#     token = ''
+#     user = User.objects.get(username=request.user)
+#     token = makeid()
+#     context = {"MailToken": token, "cid": user.username}
+#     return render(request, 'kdp/email.html', context)
+#
+# def makeid():
+#     text = ""
+#     possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+#     for i in range(0, 40):
+#         text += possible[math.floor(random.random() * len(possible))]
+#     return text
