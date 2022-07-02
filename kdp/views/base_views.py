23fb8952e747ea8981a96d393ac6cc7e36b8f81d
unknown <neworld0@gmail.com>
2022-07-02 09:15:17 +0900

    ip test 7/2 09:15

--- kdp/views/base_views.py
+++ kdp/views/base_views.py
@@ -13,8 +13,8 @@ def index(request):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("kdigitalpark.kr", 443))
     print("내부 IP: ", sock.getsockname()[0])
-    req = requests.get("http://ipconfig.kr")
-    print("외부 IP: ", re.search(r'IP Address:(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1])
+    # req = requests.get("http://ipconfig.kr")
+    # print("외부 IP: ", re.search(r'IP Address:(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1])
     return render(request, 'kdp/index.html')
 
 
