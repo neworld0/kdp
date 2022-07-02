20a90f291d1a3b69db078ad744051686f738e6cc
unknown <neworld0@gmail.com>
2022-07-02 09:24:53 +0900

    ip test 7/2 09:24

--- kdp/views/base_views.py
+++ kdp/views/base_views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-import math
 import logging
 import socket
 import requests
@@ -12,10 +9,12 @@ def index(request):
     logger.info("INFO 레벨로 출력")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("kdigitalpark.kr", 443))
-    print("내부 IP: ", sock.getsockname()[0])
+    # print("내부 IP: ", sock.getsockname()[0])
+    sockname = sock.getsockname()[0]
+    context = {'Inner IP:': sockname}
     # req = requests.get("http://ipconfig.kr")
     # print("외부 IP: ", re.search(r'IP Address:(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1])
-    return render(request, 'kdp/index.html')
+    return render(request, 'kdp/index.html', context)
 
 
 
--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -16,9 +16,9 @@ logger = logging.getLogger('kdp')
 @login_required(login_url='common:login')
 def email(request):
     logger.info("INFO 레벨로 출력")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("kdigitalpark.kr", 443))
-    print("내부 IP: ", sock.getsockname()[0])
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock.connect(("kdigitalpark.kr", 443))
+    # print("내부 IP: ", sock.getsockname()[0])
     # req = requests.get("http://ipconfig.kr")
     # print("외부 IP: ", re.search(r'IP Address:(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1])
 
