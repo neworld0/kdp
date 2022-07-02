aa5be586df35094ddf8d5865fffa86d638bceda4
unknown <neworld0@gmail.com>
2022-07-02 09:43:47 +0900

    ip test 7/2 09:43

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -16,12 +16,6 @@ logger = logging.getLogger('kdp')
 @login_required(login_url='common:login')
 def email(request):
     logger.info("INFO 레벨로 출력")
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect(("kdigitalpark.kr", 443))
-    # print("내부 IP: ", sock.getsockname()[0])
-    # req = requests.get("http://ipconfig.kr")
-    # print("외부 IP: ", re.search(r'IP Address:(\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1])
-
     hr = HttpResponse('ok')
     api_key = "#kdp@1914!"
     host_domain = "koreadigitalpark.com"
