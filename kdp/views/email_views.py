186653cf31dc08d53922a49f0cb0e5caf5b30e8b
unknown <neworld0@gmail.com>
2022-07-09 17:25:52 +0900

    email.html changed 7/9 17:25

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('kdp')
 @login_required(login_url='common:login')
 def email(request):
     logger.info("INFO 레벨로 출력")
-    hr = HttpResponse('ok')
+    hr = HttpResponse()
     api_key = "#kdp@1914!"
     host_domain = "koreadigitalpark.com"
     user = User.objects.get(username=request.user)
@@ -24,9 +24,9 @@ def email(request):
     context = {"api_key": api_key, "host_domain": host_domain, "data": data}
     json_data = json.dumps(context)
     expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
     url = "https://mail.koreadigitalpark.com/mail_api/token_sso/"
-    cookies = hr.set_cookie('MailToken', token, max_age=10, expires=expiry_time, path="/", domain=host_domain)
+    cookies = hr.set_cookie("MailToken", token, max_age=10, expires=expiry_time, path="/", domain=host_domain)
     requests.post(url, data=json_data, json=json_data, headers=headers, cookies=cookies)
     # return render(request, 'kdp/email.html', context)
     return redirect('https://mail.koreadigitalpark.com/lw_api/token_sso/' + token + '?return_url=')
