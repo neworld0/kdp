49202a217d36e96bf53b9eb44b11b828defc0dfb
unknown <neworld0@gmail.com>
2022-07-09 13:26:54 +0900

    navbar.html changed 7/9 13:26

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -13,28 +13,28 @@ logger = logging.getLogger('kdp')
 
 # email sso 생성
 @login_required(login_url='common:login')
-# def email(request):
-#     logger.info("INFO 레벨로 출력")
-#     hr = HttpResponse('ok')
-#     api_key = "#kdp@1914!"
-#     host_domain = "koreadigitalpark.com"
-#     user = User.objects.get(username=request.user)
-#     token = makeid()
-#     data = {"cid": '"' + user.username + '"', "MailToken": '"' + token + '"'}
-#     context = {"api_key": api_key, "host_domain": host_domain, "data": data}
-#     json_data = json.dumps(context)
-#     expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
-#     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-#     url = "https://mail.koreadigitalpark.com/mail_api/token_sso/"
-#     cookies = hr.set_cookie('MailToken', token, max_age=10, expires=expiry_time, path="/", domain=host_domain)
-#     requests.post(url, data=json_data, json=json_data, headers=headers, cookies=cookies)
-#     return redirect('https://mail.koreadigitalpark.com/lw_api/token_sso/' + token + '?return_url=')
-
-
-@login_required(login_url='common:login')
 def email(request):
     logger.info("INFO 레벨로 출력")
-    return render(request, 'kdp/email.html')
+    hr = HttpResponse('ok')
+    api_key = "#kdp@1914!"
+    host_domain = "koreadigitalpark.com"
+    user = User.objects.get(username=request.user)
+    token = makeid()
+    data = {"cid": '"' + user.username + '"', "MailToken": '"' + token + '"'}
+    context = {"api_key": api_key, "host_domain": host_domain, "data": data}
+    json_data = json.dumps(context)
+    expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    url = "https://mail.koreadigitalpark.com/mail_api/token_sso/"
+    cookies = hr.set_cookie('MailToken', token, max_age=10, expires=expiry_time, path="/", domain=host_domain)
+    requests.post(url, data=json_data, json=json_data, headers=headers, cookies=cookies)
+    return redirect('https://mail.koreadigitalpark.com/lw_api/token_sso/' + token + '?return_url=')
+
+
+# @login_required(login_url='common:login')
+# def email(request):
+#     logger.info("INFO 레벨로 출력")
+#     return render(request, 'kdp/email.html')
 
 
 
