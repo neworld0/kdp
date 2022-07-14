68a3ff5729d65ae69acf069794395d53ce8f29f4
Peter JH. Lee <neworld0@gmail.com>
2022-07-14 14:14:58 +0900

    email_views.py modified 7/14 14:14

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -24,13 +24,28 @@ def email(request):
     context = {"api_key": api_key, "host_domain": host_domain, "data": data}
     json_data = json.dumps(context)
     expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
-    headers = {"Content-type": "multipart/form-data", "Accept": "text/plain"}
-    # headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'cisession=0a6224bbb7f9d1d1e710d20440da602094b6a347'
+    }
     url = "https://mail.koreadigitalpark.com/mail_api/token_sso/"
     cookies = hr.set_cookie("MailToken", token, max_age=10, expires=expiry_time, path="/", domain=host_domain)
     requests.post(url, data=json_data, json=json_data, headers=headers, cookies=cookies)
-    render(request, 'kdp/email.html', context)
     return redirect('https://mail.koreadigitalpark.com/lw_api/token_sso/' + token + '?return_url=')
+
+@login_required(login_url='common:login')
+def email(request):
+    logger.info("INFO 레벨로 출력")
+    # user = User.objects.get(username=request.user)
+    token = makeid()
+    payload = 'api_key=%23kdp%401914!&host_domain=koreadigitalpark.com&data=%7B%22cid%22%3A%22kdp%22%2C%20%22MailToken%22%3A%22ZJbMIL2jwoJynEpk8wmXdlhj2e2aewvsS7VDZj9C%22%7D'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'cisession=0a6224bbb7f9d1d1e710d20440da602094b6a347'
+    }
+    url = "https://mail.koreadigitalpark.com/mail_api/token_sso/"
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response, redirect('https://mail.koreadigitalpark.com/lw_api/token_sso/' + token + '?return_url=')
 
 
 def makeid():
