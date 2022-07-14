78ca2ac6b77a0ec1128886148c9311722cc6fb05
Peter JH. Lee <neworld0@gmail.com>
2022-07-14 15:09:19 +0900

    email_views.py modified 7/14 15:09

--- kdp/views/email_views.py
+++ kdp/views/email_views.py
@@ -38,7 +38,7 @@ def email(request):
     logger.info("INFO 레벨로 출력")
     user = User.objects.get(username=request.user)
     token = makeid()
-    payload = 'api_key=%23%401914!&host_domain=koreadigitalpark.com&data=%7B%22cid%22%3A%22'+ user.username +'%22%2C%20%22MailToken%22%3A%22'+ token + '%22%7D'
+    payload = 'api_key=%23kdp%401914!&host_domain=koreadigitalpark.com&data=%7B%22cid%22%3A%22'+ user.username +'%22%2C%20%22MailToken%22%3A%22'+ token + '%22%7D'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Cookie': 'cisession=0a6224bbb7f9d1d1e710d20440da602094b6a347'
