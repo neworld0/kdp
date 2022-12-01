57e6a748e00c200c30968ac41a2519eb9e29ac1d
Peter JH. Lee <neworld0@gmail.com>
2022-12-01 14:57:15 +0900

    prod.py changed 202212011457

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.128.141', 'kdigitalpark.kr']
+ALLOWED_HOSTS = ['3.39.128.141', 'kdp.kr', 'www.kdp.kr']
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 DEBUG = False
