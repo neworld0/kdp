f19cc068a62a10c9b65df1153251a35721f3457c
unknown <neworld0@gmail.com>
2023-09-27 15:40:13 +0900

    20230926 15:40

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.128.141', 'kdp.kr', 'www.kdp.kr']
+ALLOWED_HOSTS = ['3.39.128.141', 'kdvalley.com', 'www.kdvalley.com']
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 DEBUG = False
